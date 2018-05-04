dfb309deb4d2df4427441e9b51a624d50cb16da4
Paul Mooney <paul@daishisystems.com>
2018-05-04 16:32:30 +0100

    Added DataSegmentor and associated tests.

---
+++ src/Eshopworld.Strada.Clients/Core/DataSegmentor.cs
@@ -0,0 +1,10 @@
+﻿namespace Eshopworld.Strada.Clients.Core
+{
+    public static class DataSegmentor
+    {
+        public static float CalculateSizeInKilobytes(string data)
+        {
+            return data.Length / 1024f;
+        }
+    }
+}
---
+++ test/EshopWorld.Strada.Tests/DataSegmentorTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Eshopworld.Strada.Clients.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EshopWorld.Strada.Tests
+{
+    [TestClass]
+    public class DataSegmentorTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"JsonTest.json")]
+        public void CorrectSizeInKilobytesIsCalculated()
+        {
+            // var sampleFile = File.ReadAllText(@"JsonTest.json");
+            var sampleFile = Resources.SampleFile;
+            var fileSizeInKilobytes = DataSegmentor.CalculateSizeInKilobytes(sampleFile);
+
+            Assert.AreEqual(1.544921875, fileSizeInKilobytes);
+        }
+    }
+}
