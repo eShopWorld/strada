e9966307276c2c3772195567c34b62db63509225
Paul Mooney <daishisystems@users.noreply.github.com>
2018-08-29 11:51:36 +0100

    Added sample ASP.NET Core 2.0 web app

---
+++ src/Eshopworld.Strada.Web/Controllers/CorrelationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eshopworld.Strada.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CorrelationController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var gotCorrelationId = Request.GetCorrelationId("correlationid", out var correlationId);
+            return gotCorrelationId ? correlationId : string.Empty;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
