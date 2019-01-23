05b7713eec21f51dc475485dec7eeafbd271f6f9
Paul Mooney <daishisystems@users.noreply.github.com>
2019-01-23 14:45:10 +0000

    Added .Net Core middleware lib

---
+++ src/Eshopworld.Strada.Plugins.Streaming.NetCore/DataAnalyticsMeta.cs
@@ -0,0 +1,13 @@
+﻿namespace Eshopworld.Strada.Plugins.Streaming.NetCore
+{
+    /// <summary>
+    ///     DataAnalyticsMeta provides metadata necessary for downstream analysis.
+    /// </summary>
+    public class DataAnalyticsMeta
+    {
+        /// <summary>
+        ///     Used to link related metadata in the downstream data lake.
+        /// </summary>
+        public string CorrelationId { get; set; }
+    }
+}
