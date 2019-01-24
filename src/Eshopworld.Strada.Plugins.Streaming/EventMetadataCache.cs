4078f47bef049b56e1281340efd6a5169ce45ec4
Paul Mooney <daishisystems@users.noreply.github.com>
2019-01-24 16:29:28 +0000

    Added EventMetadataCache w/ simple in-memory functionality

---
+++ src/Eshopworld.Strada.Plugins.Streaming/EventMetadataCache.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Eshopworld.Strada.Plugins.Streaming
+{
+    public class EventMetadataCache
+    {
+        private ConcurrentQueue<string> _cache;
+
+        public EventMetadataCache()
+        {
+            _cache = new ConcurrentQueue<string>();
+        }
+
+        public void Add(string eventMetadataPayload)
+        {
+            if (string.IsNullOrEmpty(eventMetadataPayload))
+                throw new ArgumentNullException(nameof(eventMetadataPayload));
+
+            if (_cache == null) _cache = new ConcurrentQueue<string>();
+            _cache.Enqueue(eventMetadataPayload);
+        }
+
+        public List<string>
+            GetEventMetadataPayloadBatch(
+                int maxItemsToRemove = int.MaxValue) // todo: param should equal batch max items.
+        {
+            if (_cache == null || _cache.IsEmpty) return new List<string>();
+
+            var eventMetadataPayloadBatch = new List<string>();
+            var counter = 0;
+            bool canDequeue;
+
+            do
+            {
+                canDequeue = _cache.TryDequeue(out var eventMetadataPayload);
+                if (canDequeue) eventMetadataPayloadBatch.Add(eventMetadataPayload);
+                counter++;
+            } while (counter < maxItemsToRemove && canDequeue);
+
+            return eventMetadataPayloadBatch;
+        }
+    }
+}
---
+++ test/Eshopworld.Strada.Plugins.Streaming.Tests/Unit/EventMetadataCacheTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Eshopworld.Strada.Plugins.Streaming.Tests.Unit
+{
+    public class EventMetadataCacheTests
+    {
+        [Fact]
+        public void AllEventMetadataPayloadsAreDequeued()
+        {
+            var eventMetadataCache = new EventMetadataCache();
+
+            for (var i = 0; i < 100; i++) eventMetadataCache.Add("EVENTMETADATAPAYLOAD");
+
+            var eventMetadataPayloadBatch = eventMetadataCache.GetEventMetadataPayloadBatch();
+            Assert.Equal(100, eventMetadataPayloadBatch.Count);
+        }
+
+        [Fact]
+        public void Top50EventMetadataPayloadsAreDequeued()
+        {
+            var eventMetadataCache = new EventMetadataCache();
+
+            for (var i = 0; i < 100; i++) eventMetadataCache.Add("EVENTMETADATAPAYLOAD");
+
+            var eventMetadataPayloadBatch = eventMetadataCache.GetEventMetadataPayloadBatch(50);
+            Assert.Equal(50, eventMetadataPayloadBatch.Count);
+        }
+
+        // TODO: Concurrency tests?
+    }
+}
