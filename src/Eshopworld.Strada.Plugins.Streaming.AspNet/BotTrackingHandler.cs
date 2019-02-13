d5592f5bc361c36888dc3e82787025729c45061e
Paul Mooney <daishisystems@users.noreply.github.com>
2019-02-13 11:07:15 +0000

    Added ASP.NET library project

---
+++ src/Eshopworld.Strada.Plugins.Streaming.AspNet/BotTrackingHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshopworld.Strada.Plugins.Streaming.AspNet
+{
+    public class BotTrackingHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            if (request.RequestUri.Segments.Contains("values"))
+                try
+                {
+                    var requestBody = await request.Content.ReadAsStringAsync();
+                    EventMetadataCache.Instance.Add(requestBody);
+                }
+                catch
+                {
+                    // ignored
+                }
+
+            var result = await base.SendAsync(request, cancellationToken);
+            return result;
+        }
+    }
+}
