aa613b8952a6f7c75ac5bd216d521d1bf56b0ee5
Paul Mooney <daishisystems@users.noreply.github.com>
2019-02-15 10:42:59 +0000

    Added DataTransmissionHandler Unit tests

--- src/Eshopworld.Strada.Plugins.Streaming.AspNet/DataTransmissionHandler.cs
+++ src/Eshopworld.Strada.Plugins.Streaming.AspNet/DataTransmissionHandler.cs
@@ -1,58 +1,27 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Eshopworld.Strada.Plugins.Streaming.AspNet
 {
-    // todo: Unit test
     public class DataTransmissionHandler : DelegatingHandler // todo: Need a remote kill-switch
     {
         protected override async Task<HttpResponseMessage> SendAsync( // todo: Consider caching results. 
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            var uriSegmentFound = false;
-            var counter = 0;
-            string uriSegmentName = null;
-
-            if (UriSegmentMetaCache.Instance.UriSegmentMeta == null)
-                return await base.SendAsync(request, cancellationToken);
-
-            var uriSegments = new List<string>(request.RequestUri.Segments);
-
-            try
-            {
-                do
-                {
-                    uriSegmentName = uriSegments[counter];
-                    if (UriSegmentMetaCache.Instance.UriSegmentMeta.Any(meta => meta.UriSegmentName == uriSegmentName))
-                        uriSegmentFound = true;
-                } while (!uriSegmentFound && ++counter < uriSegments.Count);
-            }
-            catch
-            {
-                // ignored
-            }
+            var uriSegmentFound = Functions.UriSegmentExists(
+                request.RequestUri.Segments,
+                UriSegmentMetaCache.Instance.UriSegmentMeta,
+                out var allowedHttpMethods);
 
             if (!uriSegmentFound) return await base.SendAsync(request, cancellationToken);
-
-            var allowedHttpMethods = UriSegmentMetaCache.Instance.UriSegmentMeta
-                .First(meta => meta.UriSegmentName == uriSegmentName).AllowedHttpMethods;
 
             if (!allowedHttpMethods.Contains(request.Method.Method))
                 return await base.SendAsync(request, cancellationToken);
 
-            try
-            {
-                var requestBody = await request.Content.ReadAsStringAsync();
-                if (!string.IsNullOrEmpty(requestBody)) EventMetadataCache.Instance.Add(requestBody);
-            }
-            catch
-            {
-                // ignored
-            }
+            var requestBody = await request.Content.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(requestBody)) EventMetadataCache.Instance.Add(requestBody);
 
             return await base.SendAsync(request, cancellationToken);
         }
--- src/Eshopworld.Strada.Plugins.Streaming/Functions.cs
+++ src/Eshopworld.Strada.Plugins.Streaming/Functions.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace Eshopworld.Strada.Plugins.Streaming
 {
@@ -35,6 +38,46 @@ namespace Eshopworld.Strada.Plugins.Streaming
             jsonObject.Add(new JProperty("queryString", queryString));
             jsonObject.Add(new JProperty("created", timestamp));
             return jsonObject.ToString();
+        }
+
+
+        public static bool UriSegmentExists(
+            string[] uriSegments,
+            List<UriSegmentMeta> uriSegmentMeta,
+            out HashSet<string> allowedHttpMethods)
+        {
+            if (uriSegments == null) throw new ArgumentNullException(nameof(uriSegments));
+            if (uriSegmentMeta == null) throw new ArgumentNullException(nameof(uriSegmentMeta));
+
+            var uriSegmentFound = false;
+            var index = 0;
+            string uriSegmentName;
+
+            try
+            {
+                do
+                {
+                    uriSegmentName = uriSegments[index];
+                    if (uriSegmentMeta.Any(meta => meta.UriSegmentName == uriSegmentName))
+                        uriSegmentFound = true;
+                } while (!uriSegmentFound && ++index < uriSegments.Length);
+
+
+                if (uriSegmentFound)
+                {
+                    allowedHttpMethods = uriSegmentMeta.First(meta => meta.UriSegmentName == uriSegmentName)
+                        .AllowedHttpMethods;
+                    return true;
+                }
+
+                allowedHttpMethods = null;
+                return false;
+            }
+            catch
+            {
+                allowedHttpMethods = null;
+                return false;
+            }
         }
     }
 }
