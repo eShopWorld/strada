d2dffb23eafb24d772e51d15be485b514b14e6f6
Paul Mooney <daishisystems@users.noreply.github.com>
2019-03-12 12:24:05 +0000

    Fixed missing Quartz reference issue w/ DataUploader ASPNET

---
+++ src/Eshopworld.Strada.Plugins.Streaming.AspNet/DataUploader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace Eshopworld.Strada.Plugins.Streaming.AspNet
+{
+    public class DataUploader
+    {
+        private static readonly Lazy<DataUploader> InnerDataUploader =
+            new Lazy<DataUploader>(() => new DataUploader());
+
+        private IScheduler _scheduler;
+
+        public static DataUploader Instance => InnerDataUploader.Value;
+
+        public async Task StartAsync(
+            DataTransmissionClient dataTransmissionClient,
+            EventMetaCache eventMetaCache,
+            int executionTimeInterval)
+        {
+            var factory = new StdSchedulerFactory();
+            _scheduler = await factory.GetScheduler();
+
+            await _scheduler.Start();
+
+            var jobDetail = new JobDetailImpl(
+                "eventMetadataUploadJob",
+                typeof(EventMetadataUploadJob))
+            {
+                JobDataMap =
+                {
+                    [nameof(DataTransmissionClient)] = dataTransmissionClient,
+                    [nameof(EventMetaCache)] = eventMetaCache
+                }
+            };
+
+            var trigger = TriggerBuilder.Create()
+                .WithSimpleSchedule(s => s
+                    .WithIntervalInSeconds(executionTimeInterval)
+                    .RepeatForever())
+                .Build();
+
+            await _scheduler.ScheduleJob(jobDetail, trigger);
+        }
+    }
+}
