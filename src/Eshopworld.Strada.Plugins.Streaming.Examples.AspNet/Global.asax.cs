ab78f6274e3240cce9c43aff479be185e6a1d1bc
Paul Mooney <daishisystems@users.noreply.github.com>
2019-04-02 15:52:45 +0100

    Updated GCP packages, resources

--- src/Eshopworld.Strada.Plugins.Streaming.Examples.AspNet/Global.asax.cs
+++ src/Eshopworld.Strada.Plugins.Streaming.Examples.AspNet/Global.asax.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Eshopworld.Strada.Plugins.Streaming.NetFramework;
+using Newtonsoft.Json;
 
 namespace Eshopworld.Strada.Plugins.Streaming.Examples.AspNet
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -18,6 +20,41 @@ namespace Eshopworld.Strada.Plugins.Streaming.Examples.AspNet
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // OPTIONAL: Register the Autofac model binder provider.
+            builder.RegisterWebApiModelBinderProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            var gcpServiceCredentials =
+                JsonConvert.DeserializeObject<CloudServiceCredentials>(Resources.GcpServiceCredentials);
+
+            var dataTransmissionClientConfigSettings =
+                JsonConvert.DeserializeObject<DataTransmissionClientConfigSettings>(
+                    Resources.DataTransmissionClientConfigSettings);
+
+            DataTransmissionClient.Instance.InitAsync(
+                gcpServiceCredentials,
+                dataTransmissionClientConfigSettings
+            ).Wait();
+
+            DataUploader.Instance.StartAsync(
+                DataTransmissionClient.Instance,
+                EventMetaCache.Instance,
+                dataTransmissionClientConfigSettings).Wait();
         }
     }
 }
